d62d8f41b7046285048c053acd4cbc8e1a263e36
Rustam <rustam.95.ur@gmail.com>
2019-10-16 02:00:26 +0600

    fix store image

--- app/Http/Requests/StoreImage.php
+++ app/Http/Requests/StoreImage.php
@@ -24,7 +24,7 @@ class StoreImage extends FormRequest
     public function rules()
     {
         return [
-            'image'         => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'image'         => 'image|mimes:jpeg,png,jpg,gif,svg',
         ];
     }
 }
